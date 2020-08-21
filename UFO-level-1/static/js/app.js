479459715ad4fc455919cfebb3d829704b63b8fe
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-08-20 22:35:39 -0400

    almost completed level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -10,11 +10,55 @@ tableData.forEach(function(ufoReport) {
       console.log(ufoReport);
       var row = tbody.append("tr");
 
-Object.entries(ufoReport).forEach(function([key, value]) {
+      Object.entries(ufoReport).forEach(function([key, value]) {
         console.log(key, value);
-        // Append a cell to the row for each value
-        // in the weather report object
         var cell = row.append("td");
         cell.text(value);
       });
 });
+
+
+      // grab references to the input form and the output button
+      var form = d3.select(".form-control");
+      var button = d3.select("#filter-btn");
+
+      // Create event handlers for clicking the button or pressing the enter key
+      form.on("submit", runEnter);
+      button.on("click", runEnter);
+
+
+      // Create the function to run for both events
+      function runEnter() {
+
+            // Prevent the page from refreshing
+            d3.event.preventDefault();
+
+            // Select the input element and get the raw HTML node
+            var inputElement = d3.select("#datetime");
+
+            // Get the value property of the input element
+            var inputValue = inputElement.property("value");
+
+            console.log(inputValue);
+            console.log(tableData);
+
+            var filteredData = tableData.filter(dateOutput => dateOutput.datetime === inputValue);
+
+            console.log(filteredData);
+
+
+            // Display filtered data 
+            filteredData.forEach(function(filteredReport) {
+                  console.log(filteredReport);
+                  var row = tbody.append("tr");
+
+                  Object.entries(filteredReport).forEach(function([key, value]) {
+                        console.log(key, value);
+                        var cell = row.append("td");
+                        cell.text(value);
+                  });   
+            });
+      };
+
+
+      
