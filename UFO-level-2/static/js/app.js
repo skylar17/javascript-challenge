07084017cabf2dbf0e0e7289437423d52fab7ea3
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-08-22 17:45:01 -0400

    level-2 error in filtering data

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,45 +2,74 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
 
-// --------------------------------------------------
+console.log(tableData);
 
-// Connect dropdown items to respective columns
-var dropDown = d3.select("#column-selection").on("click", funX);
+tableData.forEach(function(ufoReport) {
+      console.log(ufoReport);
+      var row = tbody.append("tr");
 
-function funX() {
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-
-    // Select the input element and get the raw HTML node
-    var inputSelection = d3.select("#column-selection").property("value");
-    // console.log(inputSelection);
-
-    // var columnSelect = d3.select("#ufo-table>table-head").text();
-
-
-    // if (inputColumn === columnSelect) {
-    //     result = 'positive';} 
-    //     else {
-    //         result = 'NOT positive';
-    //     }
-    // return result;
-
-    // console.log(inputColumn);
-
-    // var chooseColumn = tableData.filter(dropDownOutput); 
-    
-    // function dropDownOutput(){
-    //     var columnSelect = d3.select("#ufo-table>table-head").text();
-
-    //     columnSelect === chooseColumn;
-    //     console.log("columnSelect");
-    //     console.log("--------------------------------------");
-    // };
-    
-
-};
+      Object.entries(ufoReport).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+      });
+});
 
 
-// --------------------------------------------------
+      // grab references to the input form and the output button
+      var form = d3.select(".form-control");
+      var button = d3.select("#filter-btn");
 
+      // Create event handlers for clicking the button or pressing the enter key
+      form.on("submit", runEnter);
+      button.on("click", runEnter);
+
+
+      // Create the function to run for both events
+      function runEnter() {
+
+            // Prevent the page from refreshing
+            d3.event.preventDefault();
+
+            // Connect dropdown items to respective columns
+            var dropDownSelection = d3.select("#column-selection").property("value");
+            // console.log(dropDownSelection);
+
+            // Select the input element and get the raw HTML node and the value property
+            var inputValue = d3.select("#inputRowData").property("value");
+
+            // console.log(inputValue);
+
+            // console.log(tableData);
+
+
+            
+            // var filteredData = tableData.filter(output => output.inputRowData === inputValue);
+            // console.log("--------------------------------------")
+            // console.log(filteredData);
+
+            // var filteredData = tableData
+            if (dropDownSelection && inputValue) {
+                var filteredData = tableData.filter(output => output.column-selection === dropDownSelection && output.inputRowData === inputValue);
+            }
+            console.log("--------------------------------------")
+
+            tbody.html("");
+
+            // // Display filtered data 
+            // filteredData.forEach(function(filteredReport) {
+            //       console.log(filteredReport);
+            //       var row = tbody.append("tr");
+
+            //       Object.entries(filteredReport).forEach(function([key, value]) {
+            //             console.log(key, value);
+            //             var cell = row.append("td");
+            //             cell.text(value);
+            //       });   
+            // });
+      };
+
+
+      
